09058b86c804a0e13f6b361d95530e1459e54ec0
Nhan Phan <phannhan.dn@gmail.com>
2022-08-07 22:25:20 +0700

    update

---
+++ vue3-typescript/src/views/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <h5 class="text-center">
+    Ops - the cat's couldn't find what you are looking for.
+  </h5>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageNotFound",
+});
+</script>
